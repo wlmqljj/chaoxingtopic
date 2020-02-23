ce381ff5b84fc5a355e0bac5991916583e1f3254
Effort <1418045110@qq.com>
2020-02-23 10:27:14 +0800

    新增错误页面跳转

---
+++ src/main/java/site/ccczg/chaoxingtopic/handler/ControllerExceptionHandler.java
@@ -0,0 +1,35 @@
+package site.ccczg.chaoxingtopic.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author: Effort
+ * @date: 2020-02-06 15:03
+ * @description:
+ */
+public class ControllerExceptionHandler  {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView exceptionHander(HttpServletRequest request, Exception e) throws Exception {
+        logger.error("Requst URL : {}，Exception : {}", request.getRequestURL(),e);
+
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("url",request.getRequestURL());
+        mv.addObject("exception", e);
+        mv.setViewName("error/error");
+        return mv;
+    }
+}
